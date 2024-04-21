520594407b47f712e492a7757d01dcd8b4246bda
Aliaksandr Tserashkou <atserashkou@gmail.com>
2024-04-21 16:08:42 +0200

    271 Encode and Decode Strings - Done (kind of)

--- solutions/Problems/Medium/271_Encode_and_Decode_Strings.swift
+++ solutions/Problems/Medium/271_Encode_and_Decode_Strings.swift
@@ -21,10 +21,101 @@
 
 import Foundation
 
+// Option 1 - incomplete solution with bugs. O(n)
+
 func encodeStr(_ strs: [String]) -> String {
-    return ""
+    var encodedStr = ""
+    for str in strs {
+        encodedStr.append("\(str.count)|\(str)")
+    }
+    return encodedStr
 }
 
 func decodeStr(_ string: String) -> [String] {
-    return []
+    var decoded: [String] = []
+    var word = ""
+    var count = ""
+    var countInt: Int = 0
+    for (index, ch) in string.enumerated() {
+        if ch.isNumber {
+            count.append(ch)
+        } else if ch == "|" && string[index - 1] == count.last {
+            countInt = Int(count) ?? 0
+        } else {
+            word.append(ch)
+            countInt -= 1
+
+            if countInt == 0 {
+                decoded.append(word)
+                word = ""
+                count = ""
+                countInt = 0
+            }
+        }
+    }
+    return decoded
+}
+
+extension String {
+    public subscript(_ idx: Int) -> Character {
+        self[self.index(self.startIndex, offsetBy: idx)]
+    }
+}
+
+// Option 2 - solution from neetcode. O(n)
+
+func encodeStr2(_ strs: [String]) -> String {
+    if strs.isEmpty { return "#" }
+    var counts = [String]()
+    for str in strs {
+        counts.append("\(str.count)")
+    }
+    return counts.joined(separator: ",") + "#" + strs.joined()
+}
+
+func decodeStr2(_ s: String) -> [String] {
+    if s == "#" { return [] }
+    let index = s.firstIndex(of: "#")!
+    let counts = String(s[s.startIndex...s.index(before: index)]).components(separatedBy: ",")
+    var sIndex = s.index(after: index)
+    var decodedStrings = [String]()
+    for count in counts {
+        let endIndex = s.index(sIndex, offsetBy: Int(count)! - 1)
+        if sIndex > endIndex {
+            decodedStrings.append("")
+            continue
+        }
+        decodedStrings.append(String(s[sIndex...endIndex]))
+        sIndex = s.index(after: endIndex)
+    }
+    return decodedStrings
+}
+
+// Option 3 - my 2nd take. O(n)
+
+func encodeStr3(_ strs: [String]) -> String {
+    if strs.isEmpty { return "|" }
+    var counts = [String]()
+    for str in strs {
+        counts.append("\(str.count)")
+    }
+    return counts.joined(separator: ",") + "|" + strs.joined()
+}
+
+func decodeStr3(_ s: String) -> [String] {
+    if s == "|" { return [] }
+    let index = s.firstIndex(of: "|")!
+    let counts = String(s[s.startIndex...s.index(before: index)]).components(separatedBy: ",")
+    var sIndex = s.index(after: index)
+    var decodedStrings = [String]()
+    for count in counts {
+        let endIndex = s.index(sIndex, offsetBy: Int(count)! - 1)
+        if sIndex > endIndex {
+            decodedStrings.append("")
+            continue
+        }
+        decodedStrings.append(String(s[sIndex...endIndex]))
+        sIndex = s.index(after: endIndex)
+    }
+    return decodedStrings
 }
--- solutions/ProblemsTests/Medium/271_Encode_and_Decode_Strings_Tests.swift
+++ solutions/ProblemsTests/Medium/271_Encode_and_Decode_Strings_Tests.swift
@@ -3,8 +3,70 @@ import XCTest
 @testable import leetcode
 
 class EncodeAndDecodeStringsTests: XCTestCase {
+
+    // Option 1
+
     func testEncodeAndDecodeStrings1() {
-        XCTAssertEqual(encodeStr(["lint","code","love","you"]), "")
-        XCTAssertEqual(decodeStr("123"), ["123"])
+        XCTAssertEqual(encodeStr(["lint","code","love","you"]), "4|lint4|code4|love3|you")
+        XCTAssertEqual(decodeStr("4|lint4|code4|love3|you"), ["lint", "code", "love", "you"])
+    }
+
+    func testEncodeAndDecodeStrings2() {
+        XCTAssertEqual(encodeStr(["olalalalala"]), "11|olalalalala")
+        XCTAssertEqual(decodeStr("11|olalalalala"), ["olalalalala"])
+    }
+
+    func testEncodeAndDecodeStrings3() {
+        XCTAssertEqual(encodeStr(["asd|dk","privetqwe"]), "6|asd|dk9|privetqwe")
+        XCTAssertEqual(decodeStr("6|asd|dk9|privetqwe"), ["asd|dk", "privetqwe"])
+    }
+
+    func testEncodeAndDecodeStrings4() {
+        XCTAssertEqual(encodeStr(["1|asd|dk","privetqwe"]), "8|1|asd|dk9|privetqwe")
+        XCTAssertEqual(decodeStr("8|1|asd|dk9|privetqwe"), ["1|asd|dk", "privetqwe"])
+    }
+
+    // Option 2
+
+    func testEncodeAndDecodeStrings21() {
+        XCTAssertEqual(encodeStr2(["lint","code","love","you"]), "4,4,4,3#lintcodeloveyou")
+        XCTAssertEqual(decodeStr2("4,4,4,3#lintcodeloveyou"), ["lint", "code", "love", "you"])
+    }
+
+    func testEncodeAndDecodeStrings22() {
+        XCTAssertEqual(encodeStr2(["olalalalala"]), "11#olalalalala")
+        XCTAssertEqual(decodeStr2("11#olalalalala"), ["olalalalala"])
+    }
+
+    func testEncodeAndDecodeStrings23() {
+        XCTAssertEqual(encodeStr2(["asd#dk","privetqwe"]), "6,9#asd#dkprivetqwe")
+        XCTAssertEqual(decodeStr2("6,9#asd#dkprivetqwe"), ["asd#dk", "privetqwe"])
+    }
+
+    func testEncodeAndDecodeStrings24() {
+        XCTAssertEqual(encodeStr2(["1#asd#dk","privetqwe"]), "8,9#1#asd#dkprivetqwe")
+        XCTAssertEqual(decodeStr2("8,9#1#asd#dkprivetqwe"), ["1#asd#dk", "privetqwe"])
+    }
+
+    // Option 3
+
+    func testEncodeAndDecodeStrings31() {
+        XCTAssertEqual(encodeStr3(["lint","code","love","you"]), "4,4,4,3|lintcodeloveyou")
+        XCTAssertEqual(decodeStr3("4,4,4,3|lintcodeloveyou"), ["lint", "code", "love", "you"])
+    }
+
+    func testEncodeAndDecodeStrings32() {
+        XCTAssertEqual(encodeStr3(["olalalalala"]), "11|olalalalala")
+        XCTAssertEqual(decodeStr3("11|olalalalala"), ["olalalalala"])
+    }
+
+    func testEncodeAndDecodeStrings33() {
+        XCTAssertEqual(encodeStr3(["asd|dk","privetqwe"]), "6,9|asd|dkprivetqwe")
+        XCTAssertEqual(decodeStr3("6,9|asd|dkprivetqwe"), ["asd|dk", "privetqwe"])
+    }
+
+    func testEncodeAndDecodeStrings34() {
+        XCTAssertEqual(encodeStr3(["1|asd|dk","privetqwe"]), "8,9|1|asd|dkprivetqwe")
+        XCTAssertEqual(decodeStr3("8,9|1|asd|dkprivetqwe"), ["1|asd|dk", "privetqwe"])
     }
 }
