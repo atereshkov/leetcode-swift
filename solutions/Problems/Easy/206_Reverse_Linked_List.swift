bbebc9c269e77de57d5ea40528494c9543d433a7
Aliaksandr Tserashkou <atserashkou@gmail.com>
2024-12-21 13:53:24 +0100

    206 Reverse Linked List - Done

--- solutions/Problems/Easy/206_Reverse_Linked_List.swift
+++ solutions/Problems/Easy/206_Reverse_Linked_List.swift
@@ -1,5 +1,5 @@
 /*
- Done 15.12.2024. Revisited: N/A
+ Done 21.12.2024. Revisited: N/A
 
  Given the head of a singly linked list, reverse the list, and return the reversed list.
 
@@ -29,6 +29,9 @@
   *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
   * }
   */
+
+ https://www.youtube.com/watch?v=G0_I-ZF0S38
+ Google, Facebook.
  */
 
 import Foundation
@@ -41,10 +44,36 @@ class P206 {
         return ListNode()
     }
 
-    // MARK: - Option 2. Time: O(?). Memory: O(?)
+    // MARK: - Option 2 (neetcode - iterative). Time: O(n). Memory: O(1)
 
     func reverseList2(_ head: ListNode?) -> ListNode? {
-        return ListNode()
+        var prevP: ListNode?
+        var currP: ListNode? = head
+
+        while currP != nil {
+            let tmpNext = currP?.next
+            currP?.next = prevP
+            prevP = currP // shift pointers
+            currP = tmpNext
+        }
+
+        return prevP
+    }
+
+    // MARK: - Option 2 (neetcode - recursive). Time: O(n). Memory: O(n)
+
+    func reverseList3(_ head: ListNode?) -> ListNode? {
+        // TODO debug this with -> 1, and -> 1 -> 2 cases to visually check things
+        guard let head else { return nil }
+
+        var newHead: ListNode? = head
+        if head.next != nil {
+            newHead = reverseList3(head.next)
+            head.next?.next = head
+        }
+        head.next = nil
+
+        return newHead
     }
 
 }
--- solutions/ProblemsTests/Easy/206_Reverse_Linked_ListTests.swift
+++ solutions/ProblemsTests/Easy/206_Reverse_Linked_ListTests.swift
@@ -12,9 +12,19 @@ class Reverse_Linked_ListTestsTests: XCTestCase {
         XCTAssertEqual(P206().reverseList(list), expected)
     }
 
-    // Option 2 (neetcode)
+    // Option 2 (neetcode - iterative)
 
     func test206_Reverse_Linked_ListTests2() {
-        XCTAssertEqual(true, true)
+        let list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+        let expected = ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1)))))
+        XCTAssertEqual(P206().reverseList2(list), expected)
+    }
+
+    // Option 3 (neetcode - recursive)
+
+    func test206_Reverse_Linked_ListTests3() {
+        let list = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+        let expected = ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1)))))
+        XCTAssertEqual(P206().reverseList3(list), expected)
     }
 }
